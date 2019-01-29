eaf9a597fee6e44fd4b08605588a1d76a2707b6d
calin-tesu <likareturneanu@gmail.com>
2019-01-29 14:29:59 +0200

    starter code for widget

--- app/src/main/java/com/example/android/halfside/NowPlayingWidgetProvider.java
+++ app/src/main/java/com/example/android/halfside/NowPlayingWidgetProvider.java
@@ -13,7 +13,7 @@ public class NowPlayingWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        CharSequence widgetText = context.getString(R.string.appwidget_title);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.now_playing_widget_provider);
         views.setTextViewText(R.id.appwidget_text, widgetText);
