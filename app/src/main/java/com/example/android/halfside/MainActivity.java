4a69b37902068955b8868090a2bf558d8b8189e9
calin-tesu <likareturneanu@gmail.com>
2019-02-02 12:39:15 +0200

    fix resource identifiers & strings

--- app/src/main/java/com/example/android/halfside/MainActivity.java
+++ app/src/main/java/com/example/android/halfside/MainActivity.java
@@ -80,13 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_lineup) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_schedule) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_food_drinks) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_buy_tickets) {
 
         } else if (id == R.id.nav_share) {
 
