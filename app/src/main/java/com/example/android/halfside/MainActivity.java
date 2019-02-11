57b95b4a18b52e3a67735d552c597ad5597164e7
calin-tesu <likareturneanu@gmail.com>
2019-02-11 13:26:38 +0200

    read the database

--- app/src/main/java/com/example/android/halfside/MainActivity.java
+++ app/src/main/java/com/example/android/halfside/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.halfside;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,8 +17,11 @@ import android.view.View;
 
 import com.example.android.halfside.models.ArtistUrls;
 import com.example.android.halfside.models.PerformingArtist;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -27,29 +32,51 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     // Firebase instance variables
-    private FirebaseDatabase firebaseDatabase;
+    private FirebaseDatabase artistsFirebaseDatabase;
     private DatabaseReference artistsDatabaseReference;
-    private FirebaseStorage firebaseStorage;
+    private FirebaseStorage artistsPhotoFirebaseStorage;
     private StorageReference artistPhotoStorageReference;
+    private ValueEventListener lineupListener;
+
+    private RecyclerView lineupRecyclerView;
+
+    private List<PerformingArtist> performingArtistList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lineupRecyclerView = findViewById(R.id.lineup_rv);
+
         //Initialize Firebase components
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        firebaseStorage = FirebaseStorage.getInstance();
+        artistsFirebaseDatabase = FirebaseDatabase.getInstance();
+        artistsPhotoFirebaseStorage = FirebaseStorage.getInstance();
 
-        artistsDatabaseReference = firebaseDatabase.getReference("artists");
-        artistPhotoStorageReference = firebaseStorage.getReference("artists_photo");
+        artistsDatabaseReference = artistsFirebaseDatabase.getReference("artists");
+        artistPhotoStorageReference = artistsPhotoFirebaseStorage.getReference("artists_photo");
 
-        List<PerformingArtist> performingArtistList;
+        //Add value event listener to the performing artists lineup list
+        lineupListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot childSnapshot: dataSnapshot.getChildren() ) {
+                    PerformingArtist currentArtist = dataSnapshot.getValue(PerformingArtist.class);
+                    performingArtistList.add(currentArtist);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        };
+        artistsDatabaseReference.addValueEventListener(lineupListener);
+
         /*
-        * Generate a random list of artists and push them to Firebase dataabase
-        * Should be commented out after first use
-        **/
-        performingArtistList = generateArtistsList();
+         * Generate a random list of artists and push them to Firebase dataabase
+         * Should be commented out after first use
+         * TODO DELETE this method
+         **/
+        //performingArtistList = generateArtistsList();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -71,6 +98,20 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Remove performing artists lineup value event listener
+        if (lineupListener != null) {
+            artistsDatabaseReference.removeEventListener(lineupListener);
+        }
+
+        // Clean up comments listener
+        //mAdapter.cleanupListener();
     }
 
     @Override
@@ -130,9 +171,12 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+
+
     /*
-    * Helper method to generate a list of 18 artists (3 days * 6 artists per day)
-    */
+     * Helper method to generate a list of 18 artists (3 days * 6 artists per day)
+     * TODO DELETE this method
+     */
     private List<PerformingArtist> generateArtistsList() {
         List<PerformingArtist> performingArtistList = new ArrayList<>();
 
