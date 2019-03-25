e3dd07b0f8901e22f0c13412273049db867b55d6
calin-tesu <likareturneanu@gmail.com>
2019-03-25 15:35:49 +0200

    add constants for calendar day of festival

---
+++ app/src/main/java/com/example/android/halfside/Constants.java
@@ -0,0 +1,13 @@
+package com.example.android.halfside;
+
+/**
+ * Created by Calin Tesu on 3/25/2019.
+ */
+public class Constants {
+
+    //The calendar day of festival
+    //Format must be "DD Mmm YYYY"
+    public static final String FIRST_DAY_OF_FESTIVAL = "25 Mar 2019";
+    public static final String SECOND_DAY_OF_FESTIVAL = "26 May 2019";
+    public static final String THIRD_DAY_OF_FESTIVAL = "27 May 2019";
+}
