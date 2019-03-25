006ea8d171f63f8f20a84a26db2d311580085fdb
calin-tesu <likareturneanu@gmail.com>
2019-03-25 15:35:06 +0200

    setup widget

--- app/src/main/java/com/example/android/halfside/NowPlayingWidgetProvider.java
+++ app/src/main/java/com/example/android/halfside/NowPlayingWidgetProvider.java
@@ -3,23 +3,94 @@ package com.example.android.halfside;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.widget.RemoteViews;
+
+import com.example.android.halfside.models.PerformingArtist;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class NowPlayingWidgetProvider extends AppWidgetProvider {
 
+    // Firebase instance variables
+    private static FirebaseDatabase artistsFirebaseDatabase;
+    private static DatabaseReference artistsDatabaseReference;
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
+        //Initialize Firebase components
+        artistsFirebaseDatabase = FirebaseDatabase.getInstance();
+        artistsDatabaseReference = artistsFirebaseDatabase.getReference("artists");
+
         CharSequence widgetText = context.getString(R.string.appwidget_title);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.now_playing_widget_provider);
+        final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.now_playing_widget_provider);
         views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("DD Mmm YYYY");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH");
+        //Get the current time and date and format it as above
+        String currentDay = dateFormat.format(Calendar.getInstance().getTime());
+        String currentTime = timeFormat.format(Calendar.getInstance().getTime());
+        String artistName;
+
+
+        switch (currentDay) {
+            case Constants.FIRST_DAY_OF_FESTIVAL:
+                artistName = queryFirebaseDatabase("day-1", currentTime + ":00");
+                views.setTextViewText(R.id.artist_name, artistName);
+                break;
+            case Constants.SECOND_DAY_OF_FESTIVAL:
+                artistName = queryFirebaseDatabase("day-2", currentTime + ":00");
+                views.setTextViewText(R.id.artist_name, artistName);
+                break;
+            case Constants.THIRD_DAY_OF_FESTIVAL:
+                artistName = queryFirebaseDatabase("day-3", currentTime + ":00");
+                views.setTextViewText(R.id.artist_name, artistName);
+                break;
+            default:
+                views.setTextViewText(R.id.artist_name, "NO SHOW!!");
+
+
+        }
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    private static String queryFirebaseDatabase(String day, String time) {
+        final PerformingArtist[] performingArtist = new PerformingArtist[1];
+
+        Query myQuery = artistsDatabaseReference
+                .child(day)
+                .child("stage-1")
+                .orderByChild("timeOfPerforming")
+                .equalTo(time);
+
+        myQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                performingArtist[0] = dataSnapshot.getValue(PerformingArtist.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //TODO do something
+            }
+        });
+
+        return performingArtist[0].getArtistName();
     }
 
     @Override
